c0c39ebc4fe090df242dc37767bb9199c6320b00
Nani-Sore-0 <urke.radule@gmail.com>
2019-01-17 18:49:26 +0100

    init :star:

---
+++ index.js
@@ -0,0 +1,131 @@
+const pitanja = {
+    0: [
+        'Sta je invencija?',
+        'Invencija predstavlja nešto novo i originalno na globalnom novou naprimer kao sto su novi proizvodi, materijali ili procesi.'
+    ],
+    1: [
+        'Sta je inovacija?',
+        'Inovacija predstavlja prakticnu primenu novog ili znacajno unapređenog proizvoda, procesa, marketinškog metoda ili nacina organizacije. Inovacija transformise invenciju na tržišno prihvatljiv proizvod ili spektar razlicitih proizvoda.'
+    ],
+    2: [
+        'Vrste inovacija?',
+        'Vrste inovacija 4P: proizvod, proces, pozicija (kuda usmeravamo proizvod), paradigma (kako uokvirujemo ono sto radimo i preispitivanje prihvaćenih ili starih uverenja).'
+    ],
+    3: [
+        'Menadžment plan?',
+        'Menadžment plan je deo biznis plana u kome se nalaze detaljne informacije o vlasniku i menadžment timu. Nijme treba da se definisu osnovi zadaci i poslovi, najznacajnije funkcije i prava i obaveze.'
+    ],
+    4: [
+        'Sta je radno mesto?',
+        'Radno mesto je najmanja i zaokružena tehnoloska i organizacijona celina u kojoj se odvija tacno određen posao.'
+    ],
+    5: [
+        'Cinioc radnog mesta?',
+        'Cinioc radnog mesta su: posao radnog mesta, sredstva za rad, prostor za rad i najvažnije čovek.'
+    ],
+    6: [
+        'Dizajn rada?',
+        'Dizajn rada predstavlja proces definisanja načina na koji će posao biti obacljen i zadataka koji će biti dodeljeni. Dimenzije dizajna rada su obim rada (broj i vrstu zadataka) i sadrzaj rada (ovlašćenja izvršioca).'
+    ],
+    7: [
+        'Organizaciona struktura prezeća?',
+        'Organizaciona struktura prezeća predstavlja dugotrajno projektovani sistem veza i odnosa između elmenata u preduzeću.'
+    ],
+    8: [
+        'Elementi org. strukture?',
+        'Elementi org. strukture: organizacije materijalnih inputa, organizacije ljudskih resursa, raščlanjivanje radnih zadataka, organizacija menadžmenta i organizacija vremenskog redosleda.'
+    ],
+    9: [
+        'Modeli org. strukture?',
+        'Modeli org. strukture: funkcijonalni, divizioni, matrični i preduzetnički.'
+    ],
+    10: [
+        'Utrošci?',
+        'Utrošci su fizički izražene količine elmenata upotrebljeni u procesu reprodukcije.'
+    ],
+    11: [
+        'Troškovi?',
+        'Troškovi su novčani izraz utrošaka.'
+    ],
+    12: [
+        'Troškovi se dele na?',
+        'Troškovi se dele na: direktne i indirektne; fiksne i varijabilne.'
+    ],
+    13: [
+        'Direktni troškovi?',
+        'Direktni troškovi imaj zajedničko mesto nastanka. Npr: materijali izrade, zarade zaposlenih, amortizacija ...'
+    ],
+    14: [
+        'Indirektni troškovi?',
+        'Indirektni troškovi ne mogu se evidentirati po mestu i nosiocu troškova. Npr: opšti troškovi upravle, opšti troškovi izrade...'
+    ],
+    15: [
+        'Fiksni troškovi?',
+        'Fiksni troškovi ostaju nepromenjeni. Npr: amortizacija, premija osiguranja...'
+    ],
+    16: [
+        'Varijabilni troškovi?',
+        'Varijabilni troškovi se menjaju u zavisnosti od obima proizvodnje. Npr: cena materijala, plate zaposlenih...'
+    ],
+    17: [
+        'Kalkulacija?',
+        'Kalkulacija je postupak izračunavanja cene koštanja. Cilj je da svaki proizvod opteretiti iznosom troškova za svoju proizvodnju.'
+    ],
+    18: [
+        'Cena?',
+        'Cena je izraz novčane vrednosti robe ili uslige na trzistu. Preko njega preduzeće ostvaruje profiti i meri trzišno učesce'
+    ],
+    19: [
+        'Metodi formiranja cene?',
+        'Metodi formiranja cene su: metod formiranja po troškovima, metod formiranja po potražnji, vrednosni pristup formiranja cene.'
+    ],
+    20: [
+        'Metod zasnovan na troškovima?',
+        'Metod zasnovan na troškovima: metod troškovni plus, marginalni metod, metod ciljne stope prinosa.'
+    ],
+    21: [
+        'Tri osnova ekonomska princia?',
+        'Tri osnova ekonomska princia: produktivnost, ekonomičnost i rentabilnost.'
+    ],
+    22: [
+        'Produktivnost?',
+        'Produktivnost je zahtev da se ostvari određenja proizvodnja u odnosu na utrošenu radnu snagu. P = Q / L'
+    ],
+    23: [
+        'Ekonomičnost?',
+        'Ekonomičnost je zahtev da se ostvari sto vise proizvoda uz sto manje troškove. Ek = Q / U'
+    ],
+    24: [
+        'Rentabilnost?',
+        'Rentabilnost je merilo uspešnosti poslovanja. Izrazava se količnikom profita i ulozenog kapitala.'
+    ],
+    25: [
+        'Menadžment totalnim kvalitetom?',
+        'Menadžment totalnim kvalitetom (TQL) obezbeđuje unapređenje preformansi celokupnog preduzeća.'
+    ],
+    26: [
+        'Poslovna izvrsnost?',
+        'Poslovna izvrsnost se definise kao izvanredna praksa menađmenta u ostvarnju rezultata svetske klase baziranim na primenana modela izvrsnosti.'
+    ],
+    27: [
+        'Model izvrsnosti?',
+        'Model izvrsnosti je alat koje pomaže organizacijama da sami izvrše merenje i utvrde gde se nalaze u odnosu na izvrsnost.'
+    ],
+}
+
+// bad but ok it work
+
+let num
+function random() {
+    num = Math.floor(Math.random() * Math.floor(27))
+    return num
+}
+
+function novo() {
+    document.querySelector('.pitanje').textContent = pitanja[random()][0]
+    document.querySelector('.odgovor').textContent = ''
+}
+
+function show() {
+    document.querySelector('.odgovor').textContent = pitanja[num][1]
+}
